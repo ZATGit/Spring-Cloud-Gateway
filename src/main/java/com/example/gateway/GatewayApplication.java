2ab7d07b05105f73346c6582a2272053c6629f63
ZATGit <githubemailforme@protonmail.com>
2021-05-01 21:46:09 -0400

    add edge service api gateway for generic filtering

--- src/main/java/com/example/gateway/GatewayApplication.java
+++ src/main/java/com/example/gateway/GatewayApplication.java
@@ -2,6 +2,10 @@ package com.example.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpHeaders;
 
 @SpringBootApplication
 public class GatewayApplication {
@@ -10,4 +14,19 @@ public class GatewayApplication {
 		SpringApplication.run(GatewayApplication.class, args);
 	}
 
+@Bean
+RouteLocator gateway(RouteLocatorBuilder rlb) {
+	return
+		rlb
+		.routes()
+		.route(rs -> rs
+				.path("/proxy").and().host("*.somesite.io")
+				.filters(fs -> fs
+						.addResponseHeader(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN, "*")
+						.setPath("/customers")
+				)
+				.uri("http://localhost:8080/customers")
+		)
+		.build();
+	}
 }
